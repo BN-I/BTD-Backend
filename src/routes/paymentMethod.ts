7cb78e7e26e65fd6df758fd5ec8f75296a50b40e
Zabloon Albert <zabloon.albert@cubixlabs.com>
2024-12-24 03:24:36 +0500

    fixed payment method route

--- src/routes/paymentMethod.ts
+++ src/routes/paymentMethod.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { deleteModel } from "mongoose";
-import { deletePaymentMethod } from "../controllers/paymentMethods/deletePaymentMEthod";
+import { deletePaymentMethod } from "../controllers/paymentMethods/deletePaymentMethod";
 
 const paymentMethodRouter = require("express").Router();
 const {
