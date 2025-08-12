0c5f6648e24e9a3b51ab8416c82298a205f36077
Zabloon <zabloona@gmail.com>
2025-08-13 03:37:54 +0500

    added

--- src/controllers/stripe/createPortalSession.ts
+++ src/controllers/stripe/createPortalSession.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { stripe } from "../../utils/stripeInstance";
-
+import User from "../../models/user";
 const createPortalSession = async (req: Request, res: Response) => {
   try {
+    const { id } = req.params;
+    const vendor = await User.findById(id);
     // Get the customer ID from your database based on the authenticated user
-    const customerId = "cus_SAnwAxWqyRvfGL"; // Replace with actual customer ID retrieval logic
+    const customerId = vendor?.stripeCustomerId; // Replace with actual customer ID retrieval logic
 
     const session = await stripe.billingPortal.sessions.create({
       customer: customerId,
