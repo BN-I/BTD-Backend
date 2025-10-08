ddee2f6d8dac0d70ea27b585492d1f993d4ba696
Zabloon <zabloona@gmail.com>
2025-10-08 06:46:26 +0500

    added

--- src/controllers/stripe/stripeCustomerSheetController.ts
+++ src/controllers/stripe/stripeCustomerSheetController.ts
@@ -5,31 +5,36 @@ import User from "../../models/user";
 const stripeCustomerSheetController = async (req: Request, res: Response) => {
   const { id } = req.body;
 
-  if (!id) {
-    return res.status(400).json({ message: "Invalid request" });
+  try {
+    if (!id) {
+      return res.status(400).json({ message: "Invalid request" });
+    }
+
+    const user = await User.findById(id);
+
+    if (!user?.stripeCustomerId)
+      return res
+        .status(400)
+        .json({ message: "User does not have a stripe customer id" });
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: user?.stripeCustomerId },
+      { apiVersion: "2024-12-18.acacia" }
+    );
+
+    const setupIntent = await stripe.setupIntents.create({
+      customer: user?.stripeCustomerId,
+    });
+
+    res.json({
+      setupIntent: setupIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: user?.stripeCustomerId,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
   }
-
-  const user = await User.findById(id);
-
-  if (!user?.stripeCustomerId)
-    return res
-      .status(400)
-      .json({ message: "User does not have a stripe customer id" });
-
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: user?.stripeCustomerId },
-    { apiVersion: "2024-12-18.acacia" }
-  );
-
-  const setupIntent = await stripe.setupIntents.create({
-    customer: user?.stripeCustomerId,
-  });
-
-  res.json({
-    setupIntent: setupIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: user?.stripeCustomerId,
-  });
 };
 
 module.exports = { stripeCustomerSheetController };
