e711f8dcab825fb2e14a71a36ae8adf5d62e5e29
Zabloon <zabloona@gmail.com>
2025-10-25 13:36:18 +0500

    added

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import updatePackage from "../controllers/users/updatePackage";
 import getUsers from "../controllers/users/getUsers";
 import changeUserStatus from "../controllers/users/changeUserStatus";
+
+const deleteUser = require("../controllers/users/deleteUser");
 const updateUser = require("../controllers/users/updateUser");
 
 userRouter.get("/api/users", async (req: Request, res: Response) => {
@@ -26,5 +28,9 @@ userRouter.post(
     changeUserStatus(req, res);
   }
 );
+
+userRouter.delete("/api/user/:id", async (req: Request, res: Response) => {
+  deleteUser(req, res);
+});
 
 module.exports = userRouter;
